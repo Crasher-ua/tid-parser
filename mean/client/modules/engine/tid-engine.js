bfb894a3d9a23286eed0603ff12dceba4f0321cb
Yaroslav Kazmirchuk <k3p4@mail.ru>
2018-03-11 05:52:19 +0200

    Moved all engine functionality to angular application

--- mean/client/modules/engine/tid-engine.js
+++ mean/client/modules/engine/tid-engine.js
@@ -3,8 +3,249 @@ const TidApp = angular.module('TidApp', []);
 TidApp.directive('tidEngineForm', () => ({
     restrict: 'A',
     controller: TidEngineFormController,
-    controllerAs: 'tidEngineFormCtrl'
+    controllerAs: 'tidEngineFormCtrl',
+    link($scope, $element, $attrs, tidEngineFormCtrl) {
+        tidEngineFormCtrl.init();
+    }
 }));
 
-function TidEngineFormController() {
+TidApp.directive('tidEngineSlider', function($parse) {
+    return {
+        restrict: 'A',
+
+        //TODO: use directive with template instead
+        link($scope, $element, $attrs) {
+            applySlider();
+            applyInitialValue();
+            handleSliderChange();
+
+            function applySlider() {
+                $element.slider({
+                    ticks: getTicks(),
+                    // ticks_labels:['$0','$100','$200','$300','$400'],
+                    ticks_snap_bounds: 10
+                });
+            }
+
+            function getTicks() {
+                const ticksList = $attrs.tidEngineSlider;
+                const ticksArray = `[${ticksList}]`;
+                return $scope.$eval(ticksArray);
+            }
+
+            function applyInitialValue() {
+                setNgModelValue($attrs.sliderValue);
+            }
+
+            function handleSliderChange() {
+                $element.on('slide', function({value}) {
+                    setNgModelValue(value);
+                    $scope.$apply();
+                });
+            }
+
+            function setNgModelValue(value) {
+                $parse($attrs.ngModel).assign($scope, value);
+            }
+        }
+    };
+});
+
+//TODO: move $http to service/resource
+function TidEngineFormController($interval, $http) {
+    const vm = this;
+    let currentMode;
+    let isModeChanged = false;
+    let emptyRequestsAmount = 0;
+
+    Object.assign(vm, {
+        currentStatus: 'standby',
+        currentOffset: 0,
+        timeText: 0,
+        lastTimeVisible: false,
+        logRows: [],
+        requestsSentAmount: 0,
+        successNumber: 0,
+
+        init,
+        onStartClick,
+        onStopClick,
+        onFireClick,
+        onModeIncrementalClick,
+        onModeRecheckClick,
+        onCleanLogClick
+    });
+
+    function init() {
+        $interval(handleEachSecond, 1000);
+        onModeIncrementalClick();
+    }
+
+    function handleEachSecond() {
+        incrementTimeIfNeeded();
+        triggerReturnValueIfNeeded();
+    }
+
+    function incrementTimeIfNeeded() {
+        vm.timeText = isAnyButtonActive()
+            ? vm.timeText + 1
+            : 0;
+    }
+
+    function isAnyButtonActive() {
+        const buttonsActivity = [vm.startActive, vm.fireActive, vm.stopActive];
+        return buttonsActivity.some(Boolean);
+    }
+
+    function triggerReturnValueIfNeeded() {
+        if (vm.timeText > vm.waitTime) {
+            returnValue();
+        }
+    }
+
+    function onStartClick() {
+        fetchTidData();
+
+        vm.startActive = true;
+        vm.stopDisabled = false;
+        vm.fireDisabled = true;
+        vm.currentStatus = 'ongoing';
+    }
+
+    function onStopClick() {
+        vm.stopActive = true;
+        vm.startActive = false;
+        vm.currentStatus = 'stopped, waiting response';
+    }
+
+    function onFireClick() {
+        fetchTidData();
+
+        vm.fireActive = true;
+        vm.startDisabled = true;
+        vm.currentStatus = 'fired, waiting response';
+    }
+
+    function onModeIncrementalClick() {
+        vm.modeIncrementalActive = true;
+        vm.modeRecheckActive = false;
+        changeMode('incremental');
+    }
+
+    function onModeRecheckClick() {
+        vm.modeRecheckActive = true;
+        vm.modeIncrementalActive = false;
+        changeMode('recheck');
+    }
+
+    function changeMode(newMode) {
+        currentMode = newMode;
+        console.log(currentMode); //TODO: remove
+
+        if (!isAnyButtonActive()) {
+            vm.currentOffset = 0;
+        } else {
+            isModeChanged = true;
+        }
+    }
+
+    function onCleanLogClick() {
+        logRows.length = 0;
+    }
+
+    function resetTime(){
+        vm.lastTime = vm.timeText;
+        vm.timeText = 0;
+        vm.lastTimeVisible = true;
+    }
+
+    function logInfo(str){
+        logRows.unshift(str);
+    }
+
+    function returnValue() {
+        if (isModeChanged) {
+            vm.currentOffset = 0;
+            isModeChanged = false;
+        } else {
+            vm.currentOffset++;
+        }
+
+        if (vm.fireActive) {
+            vm.fireActive = false;
+            vm.startDisabled = false;
+            resetTime();
+            vm.currentStatus = 'fired, got response';
+            return;
+        }
+
+        if (vm.stopActive) {
+            vm.stopActive = false;
+            vm.stopDisabled = true;
+            vm.fireDisabled = false;
+            resetTime();
+            vm.currentStatus = 'stopped, standby';
+            return;
+        }
+
+        if (vm.maxEmptyRequests && emptyRequestsAmount == vm.maxEmptyRequests && vm.modeIncrementalActive){
+            onModeRecheckClick();
+            logInfo(`Dropped to recheck holes mode, because limit=${vm.maxEmptyRequests} reached`);
+        }
+
+        resetTime();
+        fetchTidData();
+    }
+
+    function fetchTidData(){
+        isModeChanged = false;
+        vm.requestsSentAmount++;
+
+        const data = {
+            mode: currentMode,
+            //drop: $('#scan-drop').val(),
+            offset: vm.currentOffset
+        };
+        $http.get('core.php', data)
+            .then(({data}) => {
+                //TODO: remove try/catch
+                try {
+                    vm.successNumber += data.success_number;
+
+                    if (data.success_number) {
+                        emptyRequestsAmount = 0;
+                    } else {
+                        emptyRequestsAmount++;
+                    }
+
+                    const successList = list(data.success_urls);
+                    const allList = list(data.all_urls);
+                    logInfo(`${data.success_number} (${successList} / ${allList}), max=${data.max}`);
+
+                    if (data.offset_delta) {
+                        vm.currentOffset += data.offset_delta - 1;
+                    }
+                } catch(e) {
+                    console.log('error', e, data);
+                    logInfo('error, logged to console');
+                }
+
+                returnValue();
+            });
+    }
+
+    function list(list){
+        if (!list.length) {
+            return '';
+        }
+
+        let urls = list.split(',');
+
+        const fullUrls = urls.map((url) => {
+            const id = /\/(\d+)\.html/.exec(url)[1];
+            return `<a href="${url}">${id}</a>`;
+        });
+
+        return fullUrls.join(', ');
+    }
 }
