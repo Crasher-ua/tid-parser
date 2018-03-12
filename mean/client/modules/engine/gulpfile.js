2a7129468b4c1797c6003eb708ee1ebac2d8e5cf
Yaroslav Kazmirchuk <k3p4@mail.ru>
2018-03-12 14:32:54 +0200

    Moved JS vendors to webpack build in engine

--- mean/client/modules/engine/gulpfile.js
+++ mean/client/modules/engine/gulpfile.js
@@ -2,8 +2,24 @@ const gulp = require('gulp');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const del = require('del');
+const concat = require('gulp-concat');
 
-gulp.task('default', (callback) => {
-    del(['./build']);
+gulp.task('clean', (callback) => {
+    del(['./build'], callback);
+});
+
+gulp.task('webpack', ['clean'], (callback) => {
     webpack(webpackConfig, callback);
 });
+
+gulp.task('concat', ['webpack'], () => {
+    return gulp.src([
+            '../../vendors/jquery.js',
+            '../../vendors/bootstrap-slider.js',
+            'build/tid-engine.js'
+        ])
+        .pipe(concat('tid-engine.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['concat']);
